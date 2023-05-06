c247dcf4ee303bb9c35ddbdc7b215beabf7acd96
kdf_7 <francesco.defaveri1999@gmail.com>
2023-05-07 00:28:27 +0200

    update: getting version 2.0

---
+++ Draft/typed.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+  strings: ['$ whoami', 'kdf_7'],
+  typeSpeed: 50,
+});
--- js/main.js
+++ js/main.js
@@ -13,12 +13,3 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginRight= "0";
-}
